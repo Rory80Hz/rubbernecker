9a80f97c0438c85931a48ce26549e7bec0c67bde
Rory Hanratty <rory@pengin.pro>
2016-10-27 18:36:04 +0100

    Update Styling to match pivotal
    CSS has been changed to reflect Pivotal colours (loosely). People are
    already familiar with this scheme so it makes it easier to identify what
    the status is. I also added story ID for discussion purposes and links to
    the full details on pivotal, all the payload for discussion is there.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var async = require('async');
 function storyBones(storyDetail, membershipInfo) {
     var bones = storyDetail.map(function(story) {
         var workers = story.owner_ids.map(function(worker_id) {
-            // call in here to get the persons name
             return getPersonById(worker_id, membershipInfo);
         });
         var signOffBy = getPersonById(story.requested_by_id, membershipInfo);
@@ -41,7 +40,7 @@ function getStorySummary(res) {
                 });
             },
             function(callback) {
-                //Get the list of stories
+                //Get the list of people
                 var options = {
                     url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/memberships',
                     headers: {
@@ -73,6 +72,23 @@ function getStorySummary(res) {
                         callback("Couldn't get stories thanks to this crap: " + response, null);
                     }
                 });
+            },
+            function(callback) {
+                //Get the list of delivered stories
+                var options = {
+                    url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/stories?date_format=millis&with_state=delivered',
+                    headers: {
+                        'X-TrackerToken': res.app.get('pivotalApiKey')
+                    }
+                };
+
+                request(options, function getStories(error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        callback(null, JSON.parse(body));
+                    } else {
+                        callback("Couldn't get stories thanks to this crap: " + response, null);
+                    }
+                });
             }
         ],
         // Combine the results of the things above
@@ -82,14 +98,12 @@ function getStorySummary(res) {
             } else {
                 var startedStories = storyBones(results[0], results[1]);
                 var finishedStories = storyBones(results[2], results[1]);
-                res.render('index', { title: 'Dem Stories', story: startedStories, finishedStory: finishedStories });
+                var deliveredStories = storyBones(results[3], results[1]);
+                res.render('index', { projectId:res.app.get('pivotalProjectId'),  story: startedStories, finishedStory: finishedStories, deliveredStory: deliveredStories });
             }
-            // the results array will equal ['one','two'] even though
-            // the second function had a shorter timeout.
         });
 }
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     getStorySummary(res);
 });
