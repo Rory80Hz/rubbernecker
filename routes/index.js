0f45a46f9343ba580e3e43d10787cc20f6b34898
Rory Hanratty <rory@pengin.pro>
2016-10-27 19:05:41 +0100

    Improve code structure for readability and understandability
    I removed the need for multiple views that only differed by message and status used
    for CSS for the view.
    
    I also removed repeated code in the async parallel calls, it was the same apart from the status.
    I also renamed a lot of the methods because they were not representative of what they were doing.

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,45 @@ var router = express.Router();
 var request = require('request');
 var async = require('async');
 
-function storyBones(storyDetail, membershipInfo) {
+function getStoryViewModel(storyDetail, membershipInfo) {
     var bones = storyDetail.map(function(story) {
         var workers = story.owner_ids.map(function(worker_id) {
-            return getPersonById(worker_id, membershipInfo);
+            return mapPersonFromId(worker_id, membershipInfo);
         });
-        var signOffBy = getPersonById(story.requested_by_id, membershipInfo);
+        var signOffBy = mapPersonFromId(story.requested_by_id, membershipInfo);
         return { id: story.id, name: story.name, signOffBy: signOffBy, workers: workers }
     });
 
     return bones;
 }
 
-function getPersonById(id, membershipInfo) {
+function mapPersonFromId(id, membershipInfo) {
     var picked = membershipInfo.find(membership => membership.person.id === id);
     return picked.person.name;
+}
+
+function getStoriesByStatus(res, callback, status) {
+    //Get the list of stories
+    var options = {
+        url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/stories?date_format=millis&with_state=' + status,
+        headers: {
+            'X-TrackerToken': res.app.get('pivotalApiKey')
+        }
+    };
+
+    request(options, function getStories(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            callback(null, JSON.parse(body));
+        } else {
+            callback("Couldn't get stories thanks to this crap: " + response, null);
+        }
+    });
 }
 
 function getStorySummary(res) {
     async.parallel([
             function(callback) {
-                //Get the list of stories
-                var options = {
-                    url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/stories?date_format=millis&with_state=started',
-                    headers: {
-                        'X-TrackerToken': res.app.get('pivotalApiKey')
-                    }
-                };
-
-                request(options, function getStories(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        callback(null, JSON.parse(body));
-                    } else {
-                        callback("Couldn't get stories thanks to this crap: " + response, null);
-                    }
-                });
+                getStoriesByStatus(res, callback, "started");
             },
             function(callback) {
                 //Get the list of people
@@ -57,38 +61,10 @@ function getStorySummary(res) {
                 });
             },
             function(callback) {
-                //Get the list of finished stories
-                var options = {
-                    url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/stories?date_format=millis&with_state=finished',
-                    headers: {
-                        'X-TrackerToken': res.app.get('pivotalApiKey')
-                    }
-                };
-
-                request(options, function getStories(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        callback(null, JSON.parse(body));
-                    } else {
-                        callback("Couldn't get stories thanks to this crap: " + response, null);
-                    }
-                });
+                getStoriesByStatus(res, callback, "finished");
             },
             function(callback) {
-                //Get the list of delivered stories
-                var options = {
-                    url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/stories?date_format=millis&with_state=delivered',
-                    headers: {
-                        'X-TrackerToken': res.app.get('pivotalApiKey')
-                    }
-                };
-
-                request(options, function getStories(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        callback(null, JSON.parse(body));
-                    } else {
-                        callback("Couldn't get stories thanks to this crap: " + response, null);
-                    }
-                });
+                getStoriesByStatus(res, callback, "delivered");
             }
         ],
         // Combine the results of the things above
@@ -96,10 +72,10 @@ function getStorySummary(res) {
             if (err) {
                 res.render('damn', { message: '┬──┬◡ﾉ(° -°ﾉ)', status: err, reason: "(╯°□°）╯︵ ┻━┻" });
             } else {
-                var startedStories = storyBones(results[0], results[1]);
-                var finishedStories = storyBones(results[2], results[1]);
-                var deliveredStories = storyBones(results[3], results[1]);
-                res.render('index', { projectId:res.app.get('pivotalProjectId'),  story: startedStories, finishedStory: finishedStories, deliveredStory: deliveredStories });
+                var startedStories = getStoryViewModel(results[0], results[1]);
+                var finishedStories = getStoryViewModel(results[2], results[1]);
+                var deliveredStories = getStoryViewModel(results[3], results[1]);
+                res.render('index', { projectId: res.app.get('pivotalProjectId'), story: startedStories, finishedStory: finishedStories, deliveredStory: deliveredStories });
             }
         });
 }
