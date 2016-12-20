28d8409aaecec0aec28877e926af5eafc44544f0
Rory Hanratty <rory@pengin.pro>
2016-12-20 11:33:29 +0000

    Refactor Code
    Previously all code was in routes, this meant that it was brittle and less easy
    to add additional features. Supporting components have been moved to lib/ and
    split by functionality, for fetching stories and fetching people.
    
    Additionally as a bonus a custom Favicon has been added.

--- routes/index.js
+++ routes/index.js
@@ -1,99 +1,10 @@
 
 var express = require('express');
 var router = express.Router();
-var request = require('request');
-var async = require('async');
-
-function getStoryViewModel(storyDetail, membershipInfo) {
-    var views = storyDetail.map(function(story) {
-        var workers = story.owner_ids.map(function(worker_id) {
-            return mapPersonFromId(worker_id, membershipInfo);
-        });
-        var signOffBy = mapPersonFromId(story.requested_by_id, membershipInfo);
-        return { id: story.id, name: story.name, signOffBy: signOffBy, workers: workers }
-    });
-
-    return views;
-}
-
-function mapPersonFromId(id, membershipInfo) {
-    var picked = membershipInfo.find(membership => membership.person.id === id);
-    var name = "Unknown/Inactive User";
-    if(picked) {
-        name = picked.person.name;
-    }
-    return name;
-}
-
-function getStoriesByStatus(res, callback, status) {
-    //Get the list of stories
-    var options = {
-        url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/stories?date_format=millis&with_state=' + status,
-        headers: {
-            'X-TrackerToken': res.app.get('pivotalApiKey')
-        }
-    };
-
-    request(options, function getStories(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            callback(null, JSON.parse(body));
-        } else {
-            callback("Couldn't get stories thanks to this crap: " + response.statusCode, null);
-        }
-    });
-}
-
-function getStorySummary(res) {
-    async.parallel([
-            function(callback) {
-                getStoriesByStatus(res, callback, "started");
-            },
-            function(callback) {
-                //Get the list of people
-                var options = {
-                    url: 'https://www.pivotaltracker.com/services/v5/projects/' + res.app.get('pivotalProjectId') + '/memberships',
-                    headers: {
-                        'X-TrackerToken': res.app.get('pivotalApiKey')
-                    }
-                };
-
-                request(options, function getStories(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        callback(null, JSON.parse(body));
-                    } else {
-                        callback("Couldn't get people thanks to this crap" + response.statusCode, null);
-                    }
-                });
-            },
-            function(callback) {
-                getStoriesByStatus(res, callback, "finished");
-            },
-            function(callback) {
-                getStoriesByStatus(res, callback, "delivered");
-            },
-            function(callback) {
-                getStoriesByStatus(res, callback, "rejected");
-            }
-        ],
-        // Combine the results of the things above
-        function(err, results) {
-            if (err) {
-                res.render('damn', { message: '┬──┬◡ﾉ(° -°ﾉ)', status: err, reason: "(╯°□°）╯︵ ┻━┻" });
-            } else {
-                var startedStories = getStoryViewModel(results[0], results[1]);
-                var finishedStories = getStoryViewModel(results[2], results[1]);
-                var deliveredStories = getStoryViewModel(results[3], results[1]);
-                var rejectedStories =  getStoryViewModel(results[4], results[1]);
-                var reviewSlotsFull = res.app.get('reviewSlotsLimit') <= finishedStories.length;
-                var approveSlotsFull = res.app.get('signOffSlotsLimit') <= deliveredStories.length;
-
-                res.render('index', { projectId: res.app.get('pivotalProjectId'), story: startedStories, finishedStory: finishedStories, deliveredStory: deliveredStories, rejectedStory: rejectedStories, reviewSlotsFull: reviewSlotsFull, approveSlotsFull:approveSlotsFull });
-            }
-        });
-}
+var storyFetcher = require('../lib/storyFetcher');
 
 router.get('/', function(req, res, next) {
-    getStorySummary(res);
+    storyFetcher.getStorySummary(res);
 });
 
 module.exports = router;
